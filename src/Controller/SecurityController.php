c2deab76646e27cb826e4ae1d33c85be8ae199d3
LuckyOne117 <an.thomas@outlook.fr>
2021-04-16 12:31:46 +0200

    config page login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/inscription", name="security_inscription")
+     */
+    public function user(Request $request, EntityManagerInterface $entityManager){
+        $user = new User();
+        $userForm = $this->createForm(InscriptionType::class, $user);
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted()){
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+        return $this->render('security/inscription.html.twig', ['userForm' => $userForm->createView()]);
+    }
+}
