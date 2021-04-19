75569215f94643dc413ac78a10c8bf38fa6e4101
zefalpha <70949662+zefalpha@users.noreply.github.com>
2021-04-19 18:19:10 +0200

    InscriptionType (ajout)

---
+++ src/Form/InscriptionType.php
@@ -0,0 +1,113 @@
+<?php
+
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class InscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 30
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 30
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 30
+                ]
+            ])
+            ->add('telephone', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 15
+                ]
+            ])
+            ->add('mail', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'maxlength' => 150
+                ]
+            ])
+            ->add('motDePasse', PasswordType::class, [
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'mapped' => false
+            ])
+            ->add('newMotDePasse', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes ne sont pas les mêmes',
+                'options' => ['attr' => ['class' => 'form-control']],
+                'required' => false,
+            ])
+            ->add('isAdministrateur')
+            ->add('isActif')
+            ->add('photo', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valable'
+                    ])
+                ]
+            ])
+            ->add('campus', EntityType::class, [
+                'required' => true,
+                'class' => Campus::class,
+                'choice_label' => function ($campus) {
+                    return $campus->getNom();
+                },
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('enregistrer',SubmitType::class,[
+                'label' => 'Enregister',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
